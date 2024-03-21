eabec095a5b606cd3fd2b64c4d6bd771b244fff7
LucasRossatto <lucas.sant.anagn3@gmail.com>
2024-03-21 14:21:34 -0300

    1.1 feat  commentarios
    - adicionado comentários para simplificar o entendimento do código

--- src/jogoDaVelha/Campo.java
+++ src/jogoDaVelha/Campo.java
@@ -1,22 +1,25 @@
 package jogoDaVelha;
 
 public class Campo {
-	private char simbolo;
+	private char simbolo; // Armazena o símbolo do campo
 
+	// Construtor: define o símbolo como espaço em branco
 	public Campo() {
 		this.simbolo = ' ';
 	}
 
+	// Método para obter o símbolo do campo
 	public char getSimbolo() {
 		return simbolo;
 	}
 
+	// Método para definir o símbolo do campo
 	public void setSimbolo(char simbolo) {
+		// Verifica se o campo já foi usado
 		if (this.simbolo == ' ') {
 			this.simbolo = simbolo;
 		} else {
 			System.out.println("Campo já usado");
 		}
 	}
-
 }
--- src/jogoDaVelha/JogoDaVelha.java
+++ src/jogoDaVelha/JogoDaVelha.java
@@ -5,24 +5,35 @@ import java.util.Scanner;
 public class JogoDaVelha {
 
 	public static void main(String[] args) {
-
+		// Criando o tabuleiro do jogo
 		Campo[][] jogo = new Campo[3][3];
+		// Símbolo do jogador atual
 		char simboloAtual = 'X';
+		// Variável para controlar o jogo
 		boolean game = true;
+		// Armazena o vencedor
 		String win = "";
+		// Scanner para entrada do jogador
 		Scanner sc = new Scanner(System.in);
 
+		// Inicializando o tabuleiro
 		iniciarJogo(jogo);
 
+		// Loop principal do jogo
 		while (game) {
+			// Desenha o tabuleiro
 			desenhaJogo(jogo);
+			// Verifica se alguém ganhou
 			win = verificarWin(jogo);
+			// Se houve vencedor, mostra e termina o jogo
 			if (!win.equals("")) {
 				System.out.printf("Jogador %s Venceu %n", win);
 				break;
 			}
 			try {
+				// Pede ao jogador atual para fazer uma jogada
 				if (verificarJogada(jogo, jogar(sc, simboloAtual), simboloAtual)) {
+					// Se a jogada foi válida, muda de jogador
 					if (simboloAtual == 'X') {
 						simboloAtual = 'O';
 					} else {
@@ -32,41 +43,43 @@ public class JogoDaVelha {
 			} catch (Exception e) {
 				System.out.println("Erro");
 			}
-
 		}
+		// Mensagem de fim de jogo
 		System.out.println("Fim de Jogo");
 	}
 
+	// Desenha o tabuleiro
 	public static void desenhaJogo(Campo[][] jogo) {
 		limparTela();
 		System.out.println("   0    1    2");
-		System.out.printf("0   %c | %c | %c %n", jogo[0][0].getSimbolo(), jogo[0][1].getSimbolo(),
-				jogo[0][2].getSimbolo());
-		System.out.println("-----------------");
-		System.out.printf("1   %c | %c | %c %n", jogo[1][0].getSimbolo(), jogo[1][1].getSimbolo(),
-				jogo[1][2].getSimbolo());
-		System.out.println("-----------------");
-		System.out.printf("2   %c | %c | %c %n", jogo[2][0].getSimbolo(), jogo[2][1].getSimbolo(),
-				jogo[2][2].getSimbolo());
+		for (int i = 0; i < 3; i++) {
+			System.out.printf("%d   %c | %c | %c %n", i, jogo[i][0].getSimbolo(), jogo[i][1].getSimbolo(),
+					jogo[i][2].getSimbolo());
+			if (i < 2) {
+				System.out.println("-----------------");
+			}
+		}
 	}
 
+	// Limpa a tela
 	public static void limparTela() {
 		for (int cont = 0; cont < 30; cont++) {
 			System.out.println("");
 		}
 	}
 
+	// Pede a jogada do jogador atual
 	public static int[] jogar(Scanner sc, char simboloAtual) {
 		int p[] = new int[2];
-		System.out.printf("%s %c%n", "Quem joga: ", simboloAtual);
-		System.out.print("informa a linha: ");
+		System.out.printf("%s %c%n", "Sua vez: ", simboloAtual);
+		System.out.print("Digite a linha: ");
 		p[0] = sc.nextInt();
-		System.out.print("informa a coluna: ");
+		System.out.print("Digite a coluna: ");
 		p[1] = sc.nextInt();
 		return p;
-
 	}
 
+	// Inicializa o tabuleiro
 	public static void iniciarJogo(Campo[][] jogo) {
 		for (int l = 0; l < 3; l++) {
 			for (int c = 0; c < 3; c++) {
@@ -75,6 +88,7 @@ public class JogoDaVelha {
 		}
 	}
 
+	// Verifica se a jogada é válida e atualiza o tabuleiro
 	public static boolean verificarJogada(Campo[][] jogo, int p[], char simboloAtual) {
 		if (jogo[p[0]][p[1]].getSimbolo() == ' ') {
 			jogo[p[0]][p[1]].setSimbolo(simboloAtual);
@@ -84,7 +98,9 @@ public class JogoDaVelha {
 		}
 	}
 
+	// Verifica se alguém ganhou
 	public static String verificarWin(Campo[][] jogo) {
+		// Verifica linhas
 		for (int i = 0; i < 3; i++) {
 			if (jogo[i][0].getSimbolo() != ' ' && jogo[i][0].getSimbolo() == jogo[i][1].getSimbolo()
 					&& jogo[i][0].getSimbolo() == jogo[i][2].getSimbolo()) {
@@ -92,6 +108,7 @@ public class JogoDaVelha {
 			}
 		}
 
+		// Verifica colunas
 		for (int i = 0; i < 3; i++) {
 			if (jogo[0][i].getSimbolo() != ' ' && jogo[0][i].getSimbolo() == jogo[1][i].getSimbolo()
 					&& jogo[0][i].getSimbolo() == jogo[2][i].getSimbolo()) {
@@ -99,17 +116,19 @@ public class JogoDaVelha {
 			}
 		}
 
+		// Verifica diagonal principal
 		if (jogo[0][0].getSimbolo() != ' ' && jogo[0][0].getSimbolo() == jogo[1][1].getSimbolo()
 				&& jogo[0][0].getSimbolo() == jogo[2][2].getSimbolo()) {
 			return String.valueOf(jogo[0][0].getSimbolo());
 		}
 
+		// Verifica diagonal secundária
 		if (jogo[0][2].getSimbolo() != ' ' && jogo[0][2].getSimbolo() == jogo[1][1].getSimbolo()
 				&& jogo[0][2].getSimbolo() == jogo[2][0].getSimbolo()) {
 			return String.valueOf(jogo[0][2].getSimbolo());
 		}
 
+		// Se não houver vencedor, retorna uma string vazia
 		return "";
 	}
-
 }
